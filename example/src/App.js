6a11453e170d9754b59acbf95e77e42797cafb9f
Long Nguyen <nclong87@gmail.com>
2020-04-08 19:07:08 +0700

    Fixed minor UI issue

--- example/src/App.js
+++ example/src/App.js
@@ -7,11 +7,12 @@ export default class App extends PureComponent {
     super(props)
     console.log('constructor')
   }
+  // resourceUri: "https://cdn.bambuser.net/broadcasts/614f2ce7-a2c4-4049-8d6c-290720342cb7?da_signature_method=HMAC-SHA256&da_id=9e1b1e83-657d-7c83-b8e7-0b782ac9543a&da_timestamp=1586347328&da_static=1&da_ttl=0&da_signature=dd3cff472434ed732958d26194af64e73500d2a5b6d7fbd21c259bc238ccb09d"
 
   render () {
     return (
         <Bambuserplayer
-          src="https://cdn.bambuser.net/broadcasts/996dfc93-280d-4825-8a81-37215c5e0605?da_signature_method=HMAC-SHA256&da_id=9e1b1e83-657d-7c83-b8e7-0b782ac9543a&da_timestamp=1586342546&da_static=1&da_ttl=0&da_signature=e063b19dfe841143d0d0237fbee3c030cc69b916909012c46229131720f2618c"
+          src="https://cdn.bambuser.net/broadcasts/614f2ce7-a2c4-4049-8d6c-290720342cb7?da_signature_method=HMAC-SHA256&da_id=9e1b1e83-657d-7c83-b8e7-0b782ac9543a&da_timestamp=1586347328&da_static=1&da_ttl=0&da_signature=dd3cff472434ed732958d26194af64e73500d2a5b6d7fbd21c259bc238ccb09d"
           broadcastId="123"
           autoplay
           onEnded={() => console.log('Ended')}
